a7edd13ef25d01fefe0200aaf7b20cccaaaf54a8
edgar <edgarabramian91@gmail.com>
2024-06-01 06:01:50 +0400

    init v0.1.0

--- backend/DataBase/__init__.py
+++
@@ -1,4 +0,0 @@
-from json import load
-
-with open(r"D:\PycharmProject\ViraKran_\backend\DataBase\settings.json") as f:
-    settings = load(f)
--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
-
-from routers import CranRouter, UserRouter
-from routers.UserRouters import RentalAppRouter
+from backned.Routers import cran
 
 app = FastAPI(
     title="ViraKran",
     description="ViraKran",
     version="1.0.0",
-    docs_url="/v1/docs",
+    docs_url="/docs",
 )
 
-
-app.include_router(CranRouter.router, prefix="/Cran")
-app.include_router(UserRouter.router, prefix="/User")
+app.include_router(cran.router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Mother Fucker! This is ViraKran API!"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="192.168.0.26", port=8001, log_level="info")
-
+    uvicorn.run(app, host="127.0.0.1", port=8001, log_level="info")
